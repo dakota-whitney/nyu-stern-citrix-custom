49ab11e0f6543491f34781cbfae2cf0a44360bfb
Dakota Whitney <whitneydakota@gmail.com>
2020-11-03 15:48:10 -0500

    Modified citrix CSS and strings files

---
+++ citrix-portal-custom-strings.js
@@ -0,0 +1,10 @@
+(function ($) {
+    $.localization.customStringBundle("en", {
+        UseLightVersion: "Login to Apps@Stern",
+        YouCanUseLightVersion: "Good news, we have not detected Receiver on your Desktop!",
+        SkipToLogon: "Login to Apps@Stern",
+        SoftwareNotDetectedDetails: "Good news, we have not detected Receiver on your Desktop!",
+        Example1: "This is an example",
+        Example2: "This is another example"
+    });
+})(jQuery);
