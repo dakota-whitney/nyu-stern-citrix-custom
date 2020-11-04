e79353ba0387e1295ae0f8d38bb50b30c5d9815e
Dakota Whitney <whitneydakota@gmail.com>
2020-11-04 17:37:55 -0500

    Added custom title

--- citrix-portal-custom-strings.js
+++ citrix-portal-custom-strings.js
@@ -4,7 +4,6 @@
         YouCanUseLightVersion: "Good news, we have not detected Receiver on your Desktop!",
         SkipToLogon: "Login to Apps@Stern",
         SoftwareNotDetectedDetails: "Good news, we have not detected Receiver on your Desktop!",
-        Example1: "This is an example",
-        Example2: "This is another example"
+        AppStore: "Apps@Stern Portal"
     });
 })(jQuery);
